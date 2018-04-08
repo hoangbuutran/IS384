ec42d119ff146ea1327b81e3d7fa031ced15a70d
hoangbuutran <33544451+hoangbuutran@users.noreply.github.com>
2018-04-08 17:43:54 +0700

    grrr

--- SHOPBONGDA/SHOPBONGDA/Controllers/TrangChuController.cs
+++ SHOPBONGDA/SHOPBONGDA/Controllers/TrangChuController.cs
@@ -23,7 +23,7 @@ namespace SHOPBONGDA.Controllers
             ViewBag.DsSanPham = daoSanPham.ListSanPham();
             return View(daoLoaiSanPham.ListSanPham());
         }
-//ergergergergergergerg
+//ergergergergergergerg///////////////////////
         [ChildActionOnly]
         public ActionResult MenuLeft()
         {
